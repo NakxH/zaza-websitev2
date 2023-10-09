26c6895d3e06130a2bcd0450681c9f93b8f9432f
Nakxh <74737999+NakxH@users.noreply.github.com>
2023-10-09 22:11:05 +0100

    new kebab photo

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,21 @@ export const App = () => {
   return (
     <>
       <Nav />
-      <ImageFullWidth img="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" />
+      <ImageFullWidth img="/public/kebab.jpg" />
       <TextWithPhotoRight
         isMenu={false}
         title="About Aya"
         content={aboutUsContent}
         img="https://plus.unsplash.com/premium_photo-1663852297267-827c73e7529e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
       />
-      <ImageFullWidth img="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" />
+      <ImageFullWidth img="/public/kebab.jpg" />
       <TextWithPhotoLeft
         isMenu={true}
         title="Menu"
         img="https://plus.unsplash.com/premium_photo-1663852297267-827c73e7529e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
         content={menuContent}
       />
-      <ImageFullWidth img="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" />
+      <ImageFullWidth img="/public/kebab.jpg" />
       <Reservations title="Reservations" content={reservationContent} />
       <Footer />
     </>
